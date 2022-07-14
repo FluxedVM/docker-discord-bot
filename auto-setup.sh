36746d16c8a9c0e16757a268560b1ff0641d2e1c
D <107242203+FluxedVM@users.noreply.github.com>
2022-07-14 01:10:19 -0400

    Create auto-setup.sh

---
+++ auto-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install docker.io -y # this should be the right repository for docker
+sudo docker version
+echo Your system has been updated, and docker.io has been installed. You can confirm your version through the output above. Exiting.
